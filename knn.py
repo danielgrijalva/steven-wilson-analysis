62ce3cd35b4103cfe129cb1aff023c8e7de28be7
Juanets <jdgs.gt@gmail.com>
2017-11-28 21:13:59 -0700

    Adjusting paremeters

--- knn.py
+++ knn.py
@@ -2,17 +2,19 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.feature_selection import SelectKBest
 
 def train(X, y):
-    knn = KNeighborsClassifier(weights='distance', p=1)
+    knn = KNeighborsClassifier(n_neighbors=13, weights='distance', p=2)
     knn.fit(X, y)
 
     return knn
 
 def feature_select(X, y):
-    features = SelectKBest(k=5)
+    features = SelectKBest(k=4)
     features.fit(X, y)
     selected_idx = features.get_support(1)
+    selected_features = list(X.columns[selected_idx])
+    print('Selected features:', selected_features)
 
-    return list(X.columns[selected_idx])
+    return selected_features
 
 def test(knn, test_data, attributes):
     predictions = knn.predict(test_data[attributes])
